883cbb9aed22bcbc0ab32c2b4e647ba0f039c095
Calina Thalya <116985483+CalinaThalya@users.noreply.github.com>
2023-11-26 01:13:42 -0300

    Delete gs_java-main (1)/gs_java-main directory

--- gs_java-main (1)/gs_java-main/GS_JAVA/previnatech/src/main/java/br/com/fiap/previnatech/model/Consulta.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.previnatech.model;
-
-public class Consulta {
-	
-	
-
-}
--- gs_java-main (1)/gs_java-main/GS_JAVA/previnatech/src/main/java/br/com/fiap/previnatech/model/Hospital.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.previnatech.model;
-
-public record Hospital (
-	Long idHospital,
-	String nomeHospital,
-	String dataFundacao,
-	String especialidadeHospital
-	
-) {}
